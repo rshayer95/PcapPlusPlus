e565b720753cc4c98670505a0f34907c409da1a0
Ege Çetin <egecetin@hotmail.com.tr>
2026-08-25 19:52:57 +0300

    update pcap module

--- cmake/modules/FindPCAP.cmake
+++ cmake/modules/FindPCAP.cmake
@@ -33,16 +33,27 @@
 
 find_path(PCAP_INCLUDE_DIR NAMES pcap/pcap.h pcap.h PATH_SUFFIXES include Include)
 
-# The 64-bit Wpcap.lib is located under /x64
-if(WIN32 AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+if(CMAKE_GENERATOR_PLATFORM STREQUAL "Win32")
   #
-  # For the WinPcap and Npcap SDKs, the Lib subdirectory of the top-level directory contains 32-bit libraries. The
-  # 64-bit libraries are in the Lib/x64 directory.
+  # 32-bit x86; no need to look in subdirectories of the SDK's
+  # Lib directory for the libraries, as the libraries are in
+  # the Lib directory
   #
-  # The only way to *FORCE* CMake to look in the Lib/x64 directory without searching in the Lib directory first appears
-  # to be to set CMAKE_LIBRARY_ARCHITECTURE to "x64".
+else()
   #
-  set(CMAKE_LIBRARY_ARCHITECTURE "x64")
+  # Platform other than 32-bit x86.
+  #
+  # For the WinPcap and Npcap SDKs, the Lib subdirectory of the top-level
+  # directory contains 32-bit x86 libraries; the libraries for other
+  # platforms are in subdirectories of the Lib directory whose names
+  # are the names of the supported platforms.
+  #
+  # The only way to *FORCE* CMake to look in the appropriate
+  # subdirectory of Lib for libraries without searching in the
+  # Lib directory first appears to be to set
+  # CMAKE_LIBRARY_ARCHITECTURE to the name of the subdirectory.
+  #
+  set(CMAKE_LIBRARY_ARCHITECTURE "${CMAKE_GENERATOR_PLATFORM}")
 endif()
 
 find_library(PCAP_LIBRARY NAMES pcap wpcap)
